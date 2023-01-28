1d7a71540e8eda0e07aed42fe1648b5b57889ab0
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-01-29 05:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0:['M',1000], 
+    1:['D', 500], 
+    2:['C', 100], 
+    3:['L', 50], 
+    4:['X', 10], 
+    5:['V', 5], 
+    6:['I', 1]
+  };
+let result = "";
+ while(num){
+    if(num>=1000){
+       result += "M";
+       num -= 1000;
+    }else if(num>=500){
+       if(num>=900){
+          result += "CM";
+          num -= 900;
+       }else{
+          result += "D";
+          num -= 500;
+       }
+       }else if(num>=100){
+          if(num>=400){
+             result += "CD";
+             num -= 400;
+          }else{
+             result += "C";
+             num -= 100;
+          }
+       }else if(num>=50){
+          if(num>=90){
+             result += "XC";
+             num -= 90;
+          }else{
+             result += "L";
+             num -= 50;
+          }
+}else if(num>=10){
+          if(num>=40){
+             result += "XL";
+             num -= 40;
+          }else{
+             result += "X";
+             num -= 10;
+          }
+       }else if(num>=5){
+          if(num>=9){
+             result += "IX";
+             num -= 9;
+         }else{
+             result += "V";
+             num -= 5;
+          }
+       }else{
+          if(num>=4){
+             result += "IV";
+             num -= 4;
+       }else{
+          result += "I";
+          num -= 1;
+       }
+    }
+ }
+ return result;
 
-function romanize (num) {
-    if (isNaN(num))
-        return NaN;
-    var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "","I","II","III","IV","V","VI","VII","VIII","IX"],
-        roman = "",
-        i = 3;
-    while (i--)
-        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
-}
+//your code here
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
